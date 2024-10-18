2c14cfa8296b35799d0ac6a221d949f1677448a8
niuxinss <156888628+niuxinss@users.noreply.github.com>
2024-10-17 23:34:02 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 4 Pseudocode: Card Flip Game
+// Implementing the game
+// Author: Mandy Xie (mxie34@ucsc.edu)
+// Date: October 17th, 2024
 
-// Constants
 
-// Functions
+// -------------------Pseudocode-------------------------//
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print a grid that stays for the entire game
+  // Loop through 4 rows and 4 columns
+    // Print horizontal and vertical lines between spaces
+// Print cards to fill each slot in the grid
+  // Cards have 2 images, 1 for the back cover, and 1 for the face
+    // Back covers are all the same, faces are only in pairs
+// Randomize card faces on each card
+// Accept user cursor input
+  // Limit cursor clicks to 2
+// User can only click different cards, not the same card twice
+// When a card is clicked, change cover image to face
+// Test if the 2 card faces match
+  // If card faces match, remove cards from grid
+  // Else if card faces don’t match, return the card’s image to the back cover
+// If all cards matched and removed, user wins
