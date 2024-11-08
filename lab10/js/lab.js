e0a2984fcd09569388bd0fe38374e01b92b6fbee
niuxinss <156888628+niuxinss@users.noreply.github.com>
2024-11-07 23:30:45 -0800

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,35 @@
+/*
+
+this js file appends new elements to output div in index.html
+requirements: need jQuery loaded
+
+author: Mandy Xie
+Date: November 7th, 2024
+
+*/
+
+function randomGenerate() {
+    const text = "aRHAHRGAHRGHARGHARGHARG ARGHHH ARGHHH ARGHHH AHHHHHHHHHHHHHHHHHHHHHHH HAGRHARGARHAGRH";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    //random starting index to slice text constant
+    const randStart = Math.floor(Math.random()*(text.length - randLen + 1))
+    
+    // generate random text
+    return text.slice(randStart, randStart + randLen);
+}
+
+//click listener for button
+
+$("#convo").click(function(){
+    const newText = randomGenerate();
+    
+// append new div to output div
+    $("#output").append('<div class="text"><p>'+newText+'</p></div>');
+});
+
+
+
+
