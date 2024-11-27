5e1ab582881df153814731c728fc9753587e6eae
niuxinss <156888628+niuxinss@users.noreply.github.com>
2024-11-26 21:52:33 -0800

    debug

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -7,8 +7,18 @@
    Date: November 5th 2024
 */
 
-$("#challenges.minor-section").append("<button>buttonz");
+$("#challenges.minor-section").append("<button id='one'>buttonz");
+$("#problems.minor-section").append("<button id='two'>button!!");
+$("#results.minor-section").append("<button id ='three'>button?!");
 
-$("button").click(function(){
+$("#one").click(function(){
     $("#challenges.minor-section").toggleClass("special");
+})
+
+$("#two").click(function(){
+    $("#problems.minor-section").toggleClass("special1");
+})
+
+$("#three").click(function(){
+    $("#results.minor-section").toggleClass("special2");
 })
